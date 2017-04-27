73d3f9aecfe9b5d4a06105ea9a309def55168572
JamesG3 <emislim12@gmail.com>
2017-04-27 11:36:26 -0400

    projpost

---
+++ projPost.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+  <title>Register</title>
+</head>
+<body>
+<h1 align=center>Post your Project</h1>
+<form method="POST" action="proj_Post.php">
+
+<table align=center>
+  <tr>
+    <td>project name:</td>
+    <td><input type="text" size="20" name="pname"></td>
+  </tr>
+  <tr>
+    <td>minimum amount:</td>
+    <td><input type="text" size="20" name="mini"></td>
+  </tr>
+  <tr>
+    <td>maximum amount:</td>
+    <td><input type="text" size="20" name="maxi"></td>
+  </tr>
+  <tr>
+    <td>fund deadline:</td>
+    <td><input type="text" size="20" name="fddl"></td>
+  </tr>
+  <tr>
+    <td>project deadline:</td>
+    <td><input type="text" size="20" name="pddl"></td>
+  </tr>
+  <tr>
+    <td>category:</td>
+    <td><input type="text" size="20" name="category"></td>
+  </tr>
+  <!--需要改为radio或者下拉菜单-->
+
+
+  <tr>
+    <td>tags:</td>
+    <td><input type="text" size="20" name="tags"></td>
+  </tr>
+  <tr>
+    <td>description:</td>
+    <td><textarea id = "input" rows=5 cols=18 name="description"></textarea></td>
+  </tr>
+</table>
+
+<p align=center><input type="submit" value="post it now">
+<!--需要检查是否格式正确，所有信息必须都填写，不能为空-->
+
+</form>
+</body>
+</html>
+
+<?php
---
+++ proj_Post.php
@@ -0,0 +1,67 @@
+<?php
+    date_default_timezone_set("America/New_York");
+    $current_time = date("Y/m/d H:i:s");
+    #echo $current_time;
+
+    session_start();
+    $uid = $_SESSION["uid"];
+    #$uname = 'Fanta';
+
+    $db = new mysqli('127.0.0.1', 'root', 'root','easyfund') or die('Could not connect: ' . mysqli_error());
+
+    #get uid
+    /*
+    $getuid = "SELECT uid FROM user where username = ";
+    $getuid .= "'".$uname."'";
+    $getid = $db->query($getuid);
+    while ($row = $getid->fetch_assoc()){
+        $uid = $row['uid'];
+      }
+    */
+    
+    $input_pname = $_POST["pname"];
+    $input_mini = $_POST["mini"];
+    $input_maxi = $_POST["maxi"];
+    $input_fddl = $_POST["fddl"];
+    $input_pddl = $_POST["pddl"];
+    $input_category = $_POST["category"];
+    $input_tags = $_POST["tags"];
+    $input_descri = $_POST["description"];
+
+
+    $check1 = "SELECT pname FROM project where pname = ";
+    $check1 .= "'".$input_pname."'";
+    
+
+   	$ifexist1 = $db->query($check1);
+   	if (!$ifexist1){
+   		echo "Something wrong!!";
+   		showerror();				# if query faild, show error message.
+   	}
+
+  if(mysqli_num_rows($ifexist1)==0){
+      # insert this new user into user table
+      $Pjstate = 'incomplete';
+      $current_amount = 0;
+
+      $postpj = $db->prepare("INSERT INTO project (`pname`, `minamount`, `maxamount`, `uid`, `fundDdl`, `projDdl`, `category`, `tags`, `description`, `Pjstate`, `pjposttime`, `current_amount`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
+      $postpj->bind_param("siiisssssssi", $input_pname, $input_mini, $input_maxi, $uid, $input_fddl, $input_pddl, $input_category, $input_tags, $input_descri, $Pjstate, $current_time, $current_amount);
+      $postpj->execute();
+      echo "Your project is added!";
+
+?>
+      <form action="mainpage.php">
+      <p align=center><input type="submit" value="back to my page">
+      </form>
+<?php
+
+  }
+
+  else{
+
+    # the project name is already exist!
+    echo "This project is already exist, please change another name.";
+
+  }
+
+
